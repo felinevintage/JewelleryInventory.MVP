7596ddb3e80b62da7ca02dce4a92311cfe1d6bb4
ShonaPugh <shonapugh@gmail.com>
2024-02-06 12:28:22 +0000

    Before lunch Tuesday commit

---
+++ Codeop/WeekFour/vue-examples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- <div>Welcome to Vue</div> -->
+  <!-- 3: use the component in the template -->
+  <!-- <HelloWorld />
+  <Counter :max="20"/>
+  <Counter :max="10"/> -->
+  <Bank />
+</template>
+
+<script>
+// 1: import the component
+import HelloWorld from "./components/HelloWorld.vue";
+import Counter from "./components/Counter.vue";
+import Bank from "./components/Bank.vue";
+export default {
+  // 2: register the component
+  components: {
+    HelloWorld,
+    Counter,
+    Bank,
+  },
+};
+</script>
---
+++ Codeop/WeekFour/vue-examples/src/components/Counter.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>Count up to {{ max }}: <span :class="count <= 0 ? 'negative' : 'positive'"> {{ count }}</span></div>
+    <button @click="increase" :disabled="count ===max">+1</button>
+    <button @click="decrease" :disabled="count === 0">-1</button>
+  </template>
+  <script>
+  export default {
+    props: ("max"),
+    data() {
+      return {
+        count: 0,
+      };
+    },
+    methods: {
+        decrease() {
+            if(this.count > 0)this.count--;
+        },
+        increase() {
+            if (this.count < this.max) this.count++;
+        },
+    },
+  };
+  </script>
+
+  <style>
+  .positive{
+    color: darkgreen;
+  }
+
+  .negative {
+    color: darkred;
+  }
+
+</style>
