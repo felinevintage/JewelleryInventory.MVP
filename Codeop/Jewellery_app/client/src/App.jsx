650c2e54d2036ac95255096c63b49031c41afd66
ShonaPugh <shonapugh@gmail.com>
2024-02-26 09:21:42 +0000

    monday morning second

--- Codeop/Jewellery_app/client/src/App.jsx
+++ Codeop/Jewellery_app/client/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import './App.css'
 import Category from "./pages/Category";
 import AddItem from "./pages/AddItem";
-import itemlist from "./components/itemlist";
+import itemlist from "./pages/itemlist";
 
 
 
